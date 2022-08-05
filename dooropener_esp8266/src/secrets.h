44eac55c77ade1464de8c3fe0f2d0593ed971c5a
one0one2552 <66249155+one0one2552@users.noreply.github.com>
2022-08-05 03:48:24 +0200

    Add files via upload

---
+++ dooropener_esp8266/src/secrets.h
@@ -0,0 +1,15 @@
+#define SECRET_SSID_NOVA "101"		// replace MySSID with your WiFi network name
+#define SECRET_PASS_NOVA "1234567890"	// replace MyPassword with your WiFi password
+
+#define SECRET_SSID_101 "101"		// replace MySSID with your WiFi network name
+#define SECRET_PASS_101 "1234567890"	// replace MyPassword with your WiFi password
+
+#define SECRET_SSID_ASUS "mimimimiii"		// replace MySSID with your WiFi network name
+#define SECRET_PASS_ASUS "dontcry4meargentina"	// replace MyPassword with your WiFi password
+
+#define SECRET_BOTTOKEN_ONE0ONE_HA "5294544941:AAFkB8iSByTGshSJLgEpQGceMwCdwyHLfLs"
+#define SECRET_BOTTOKEN_NOVA_GARAGE_DOOR "5521824475:AAHCpic8kHMtc3kJuaemfRoEAVD7xv8qflM"
+
+#define SECRET_CHATID_STEFAN "849711386"
+#define SECRET_CHATID_JESS "5374528438"
+#define SECRET_CHATID_GROUP_DOOR_ROSENGARTEN "-707324753"
---
+++ esp32 door opener/README.md
@@ -0,0 +1,1 @@
+"# esp32-door-opener" 
